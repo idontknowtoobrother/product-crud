0a010eb89a466b153f98b63481c2a1e7e2fead9a
Jakkrit Chaopron <idontknowbrotherpls@gmail.com>
2022-02-28 20:51:28 +0700

    move: repo to new git account

---
+++ src/components/Menu.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <b-img src="../assets/logoBuu.png" width="80%" class="pt-2 pb-2"></b-img>
+    <b-nav vertical>
+      <b-nav-item to="/">Home</b-nav-item>
+      <b-nav-item to="/about">About</b-nav-item>
+      <b-nav-item @click="showFormInput = !showFormInput"
+        >Form Input
+        <b-icon
+          :icon="showFormInput ? 'caret-down-fill' : 'caret-right-fill'"
+        ></b-icon
+      ></b-nav-item>
+      <b-nav-item class="sub-menu" v-if="showFormInput" to="./formInput1"
+        >Form Input 1</b-nav-item
+      >
+      <b-nav-item class="sub-menu" v-if="showFormInput" to="./formInput2"
+        >Form Input 2</b-nav-item
+      >
+      <b-nav-item class="sub-menu" v-if="showFormInput" to="./formInput3"
+        >Form Input 3</b-nav-item
+      >
+      <b-nav-item class="sub-menu" v-if="showFormInput" to="./formInput4"
+        >Form Input 4</b-nav-item
+      >
+      <b-nav-item class="sub-menu" v-if="showFormInput" to="./formInput5"
+        >Form Input 5</b-nav-item
+      >
+      <b-nav-item class="sub-menu" v-if="showFormInput" to="./formInput6"
+        >Form Input 6</b-nav-item
+      >
+      <b-nav-item class="sub-menu" v-if="showFormInput" to="./formInput7"
+        >Form Input 7</b-nav-item
+      >
+      <b-nav-item class="sub-menu" v-if="showFormInput" to="./formInput8"
+        >Form Input 8</b-nav-item
+      >
+      <b-nav-item class="sub-menu" v-if="showFormInput" to="./formInput9"
+        >Form Input 9</b-nav-item
+      >
+      <b-nav-item class="sub-menu" v-if="showFormInput" to="./formInput10"
+        >Form Input 10</b-nav-item
+      >
+      <b-nav-item class="sub-menu" v-if="showFormInput" to="./formInput11"
+        >Form Input 11</b-nav-item
+      >
+
+      <b-nav-item @click="showForm = !showForm"
+        >Form
+        <b-icon
+          :icon="showForm ? 'caret-down-fill' : 'caret-right-fill'"
+        ></b-icon
+      ></b-nav-item>
+      <b-nav-item class="sub-menu" v-if="showForm" to="./form1"
+        >Form 1</b-nav-item
+      >
+      <b-nav-item class="sub-menu" v-if="showForm" to="./form2"
+        >Form 2</b-nav-item
+      >
+      <b-nav-item class="sub-menu" v-if="showForm" to="./form3"
+        >Form 3</b-nav-item
+      >
+      <b-nav-item class="sub-menu" v-if="showForm" to="./form4"
+        >Form 4</b-nav-item
+      >
+
+      <b-nav-item @click="showCrudExample = !showCrudExample"
+        >CRUD Example
+        <b-icon
+          :icon="showCrudExample ? 'caret-down-fill' : 'caret-right-fill'"
+        ></b-icon
+      ></b-nav-item>
+      <b-nav-item class="sub-menu" v-if="showCrudExample" to="./datatable1"
+        >Data table 1</b-nav-item
+      >
+      <b-nav-item class="sub-menu" v-if="showCrudExample" to="./productform1"
+        >Product Form 1</b-nav-item
+      >
+      <b-nav-item class="sub-menu" v-if="showCrudExample" to="./productTable"
+        >Product Table</b-nav-item
+      >
+    </b-nav>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      showFormInput: false,
+      showForm: false,
+      showCrudExample: false
+    }
+  }
+}
+</script>
+<style>
+.app-menu {
+  background-color: gray;
+  font-size: 10pt;
+}
+.nav-link {
+  color: white;
+  text-align: left;
+}
+.nav-link:hover {
+  color: gold;
+}
+.sub-menu {
+  padding-left: 10pt;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,120 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/formInput1',
+    name: 'FormInput1',
+    component: () => import('../views/FormInput1.vue')
+  },
+  {
+    path: '/formInput2',
+    name: 'FormInput2',
+    component: () => import('../views/FormInput2.vue')
+  },
+  {
+    path: '/formInput3',
+    name: 'FormInput3',
+    component: () => import('../views/FormInput3.vue')
+  },
+  {
+    path: '/formInput4',
+    name: 'FormInput4',
+    component: () => import('../views/FormInput4.vue')
+  },
+  {
+    path: '/formInput5',
+    name: 'FormInput5',
+    component: () => import('../views/FormInput5.vue')
+  },
+  {
+    path: '/formInput6',
+    name: 'FormInput6',
+    component: () => import('../views/FormInput6.vue')
+  },
+  {
+    path: '/formInput7',
+    name: 'FormInput7',
+    component: () => import('../views/FormInput7.vue')
+  },
+  {
+    path: '/formInput8',
+    name: 'FormInput8',
+    component: () => import('../views/FormInput8.vue')
+  },
+  {
+    path: '/formInput9',
+    name: 'FormInput9',
+    component: () => import('../views/FormInput9.vue')
+  },
+  {
+    path: '/formInput10',
+    name: 'FormInput10',
+    component: () => import('../views/FormInput10.vue')
+  },
+  {
+    path: '/formInput11',
+    name: 'FormInput11',
+    component: () => import('../views/FormInput11.vue')
+  },
+  {
+    path: '/form1',
+    name: 'Form1',
+    component: () => import('../views/Form1.vue')
+  },
+  {
+    path: '/form2',
+    name: 'Form2',
+    component: () => import('../views/Form2.vue')
+  },
+  {
+    path: '/form3',
+    name: 'Form3',
+    component: () => import('../views/Form3.vue')
+  },
+  {
+    path: '/form4',
+    name: 'Form4',
+    component: () => import('../views/Form4.vue')
+  },
+  {
+    path: '/datatable1',
+    name: 'Data Table 1',
+    component: () => import('../views/DataTable1.vue')
+  },
+  {
+    path: '/productform1',
+    name: 'Product Form 1',
+    component: () => import('../views/ProductForm1.vue')
+  },
+  {
+    path: '/productTable',
+    name: 'Product Table',
+    component: () => import('../views/product/ProductTable.vue')
+  }
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
